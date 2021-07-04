d55b0d9945877eac81f7770c190a56877948546f
Ognjen <radisicognjen@gmail.com>
2021-07-04 22:44:03 +0200

    "login" functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
 
@@ -7,16 +8,17 @@ const useStyles = makeStyles((theme) => ({
 		flexGrow: 1,
 	},
 	logo: {
-		marginRight: "30px",
+		marginRight: "50px",
 	},
-
-	title: {
-		flexGrow: 1,
+	textLink: {
+		color: "inherit",
+		textDecoration: "inherit",
 	},
 }));
 
 const Navbar = () => {
 	const classes = useStyles();
+	const [loggedIn, setLoggedIn] = useState(false);
 
 	return (
 		<div className={classes.root}>
@@ -25,12 +27,31 @@ const Navbar = () => {
 					<Typography variant="h5" className={classes.logo}>
 						Navbar
 					</Typography>
-					<Typography variant="h6" className={classes.title}>
-						Home
+
+					{/* Depending on loggedIn state, regulate flex-grow property */}
+					<Typography
+						variant="h6"
+						style={{ flexGrow: loggedIn ? 0 : 1, marginRight: "20px" }}>
+						<Link to="/" className={classes.textLink}>
+							Home
+						</Link>
 					</Typography>
-					<Button variant="contained" color="secondary">
-						Login
-					</Button>
+
+					{/*Quasi loggin functionality, when button is clicked display Profile (Nav item) permanently */}
+					{loggedIn ? (
+						<Typography variant="h6">
+							<Link to="/profile" className={classes.textLink}>
+								Profile
+							</Link>
+						</Typography>
+					) : (
+						<Button
+							variant="contained"
+							color="secondary"
+							onClick={() => setLoggedIn(true)}>
+							Login
+						</Button>
+					)}
 				</Toolbar>
 			</AppBar>
 		</div>
